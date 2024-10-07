7950df98f2f2ebdf5092525207fdb3a548aa99d3
Lawe02 <89772680+Lawe02@users.noreply.github.com>
2024-10-07 11:19:55 +0200

    feat:add html file that displays players

--- src/main/java/org/example/crudboot/Controller.java
+++ src/main/java/org/example/crudboot/Controller.java
@@ -1,14 +1,27 @@
 package org.example.crudboot;
 
+import org.example.crudboot.Model.Player;
 import org.example.crudboot.Service.ObjectService;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.List;
+
+@org.springframework.stereotype.Controller
 public class Controller {
     private ObjectService service;
 
     public Controller(ObjectService service) {
         this.service = service;
     }
+
+    @GetMapping("/")
+    public String getAllPlayers(Model model) {
+        List<Player> players = service.getAllPlayers(); // Get the list of players
+        model.addAttribute("players", players); // Add the players list to the model
+        return "index"; // Return the name of the Thymeleaf template (players.html)
+    }
+
 }
